6caf989e4722522cd38cf7765f60827636c9a9ec
Arno Hu <huzzh3@outlook.com>
2024-03-08 22:13:05 +0800

    fix: 调整网页标题以及网页语言

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+export const metadata = {
+    title: 'ECNC 排班系统'
+}
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="zh-CN">
             <body>{children}</body>
         </html>
     )
