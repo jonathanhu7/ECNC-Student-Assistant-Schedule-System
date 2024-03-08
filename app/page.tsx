1f4f78b3c47563ae71411efef8e6820440cda96b
Arno Hu <huzzh3@outlook.com>
2024-03-08 22:14:10 +0800

    feat: 在主页中添加一些元素用于练习 next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+import LikeButton from "./like-button";
+
+function Header({ title }) {
+    return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship." />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+            <LikeButton />
+        </div>
+    );
 }
