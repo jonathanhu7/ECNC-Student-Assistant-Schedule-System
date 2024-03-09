ed9ea2d034c74b8ad0b7995a3af21d034bb76271
Arno Hu <huzzh3@outlook.com>
2024-03-09 10:25:21 +0800

    feat(page): 简单应用了 tailwindcss 的样式

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,9 @@ export default function HomePage() {
                 ))}
             </ul>
             <LikeButton />
+            <h1 className="text-3xl font-bold underline">
+                Hello, world!
+            </h1>
         </div>
     );
 }
