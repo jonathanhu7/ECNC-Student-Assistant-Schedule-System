1939e465030b54722e076f22b08727438af2522f
jonathanhu7 <jonathanhu7@outlook.com>
2024-03-28 17:13:37 +0800

    feat: 完成部分登录内容

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Layout } from "@/components/auth/layout";
+import { SignInForm } from "@/components/auth/sign-in-form";
+import { GuestGuard } from "@/components/auth/guest-guard";
 
 export default function Page(): React.ReactElement {
-  return <div></div>;
+  return (
+    <GuestGuard>
+      <Layout>
+        <SignInForm />
+      </Layout>
+    </GuestGuard>
+  );
 }
---
+++ src/components/auth/sign-in-form.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+// TODO: 完成登录表单
+export function SignInForm(): React.ReactElement {
+  return <Stack></Stack>;
+}
