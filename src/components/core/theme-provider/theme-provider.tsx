5bb5579d044b417f6fc3df676c33e8218b056cd0
jonathanhu7 <jonathanhu7@outlook.com>
2024-03-28 15:46:55 +0800

    feat: 保证了样式的一致性，且使用 EmotionCache 来进行样式缓存

--- src/components/core/theme-provider/theme-provider.tsx
+++ src/components/core/theme-provider/theme-provider.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { CssBaseline } from "@mui/material"; // CssBaseline 的作用是确保应用的基础样式的一致性，通过充值浏览器默认样式来实现
-import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material"; // Experimental_CssVarsProvider 支持各种 CSS 变量，使得主题定制和动态变更更加高效
+import {
+  CssBaseline, // CssBaseline 的作用是确保应用的基础样式的一致性，通过充值浏览器默认样式来实现
+  Experimental_CssVarsProvider as CssVarsProvider, // Experimental_CssVarsProvider 支持各种 CSS 变量，使得主题定制和动态变更更加高效
+} from "@mui/material";
+import { createTheme } from "@/style/theme/create-theme";
+import EmotionCache from "./emotion-cache"; // EmotionCache 用来记录 CSS 缓存信息，可以提高速度
 
 export interface ThemeProviderProps {
   children: React.ReactNode;
@@ -9,10 +13,13 @@ export interface ThemeProviderProps {
 export function ThemeProvider({
   children,
 }: ThemeProviderProps): React.ReactElement {
+  const theme = createTheme();
   return (
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </CssVarsProvider>
+    <EmotionCache options={{ key: "mui" }}>
+      <CssVarsProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </CssVarsProvider>
+    </EmotionCache>
   );
 }
